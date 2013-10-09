6427007cc2be71674b0cdb6a6a7f133377b86354
taoyijie <taoyijie610@gmail.com>
2013-10-09 00:58:56 -0400

    half

--- src/levelunknown/P439.java
+++ src/levelunknown/P439.java
@@ -7,41 +7,26 @@ public class P439 {
 	 */
 	public static void main(String[] args) {
 		//long m = (long)1e9;
-		long cap = 4; 
-		long ons = cap*cap;
-		long sum = 0;
-		
-//		for(long i=1;i<=cap;i++){
-//		    long times = cap/i;
-//		    long tmp = times*cap + (cap-times)*times;
-//		    System.out.println(tmp);
-//		    //sum += tmp*i;
-//		}
-//		System.out.println(sum);
-//		
-//		for(long i=2;i<=cap;i++){	
-//			long first = i*(i);
-//			long last = cap*i;
-//			long terms = cap-(i-1);
-//			long f = (cap/i)*2;
-//		    long tmp = (first+last)*terms/2*(f);
-//			sum += tmp;
-//			System.out.println(i+": "+tmp);
-//		}		
-//		System.out.println(sum);
-		long tmpp = 0;
+		long cap = 10; 
+		long sum = 0;	
+
+		long base = 0;
 		for(int j=1;j<=cap;j++){
-            tmpp += (cap/j)*j;
+            base += (cap/j)*j;
         }
 		for(int i=1;i<=cap;i++){
-		    long tmp = tmpp;
-		    long s= 0;
-		    //tmp += (1+cap)*cap/2*i;
-		    for(int j=1;j<=cap;j++){
-		        long t = (cap/j-cap/(i*j));
-		        s += t*j*i;
+		    long tmp = base;
+		    long f= 0;
+		    for(int k=2;k<=i;k++){
+		    	if(i%k==0){
+		    		for(int j=1;j<=cap;j++){
+			        	long t = (cap/j-cap/(k*j));
+			        	f += t*j*k;
+			    	}
+		    		System.out.println(f);
+		    	}
 		    }
-		    tmp += s;
+		    tmp += f;
 		    System.out.println(i+": "+tmp);
 		    sum += tmp;
 		}
